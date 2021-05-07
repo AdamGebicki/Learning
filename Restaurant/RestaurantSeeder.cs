3b28dfd85cd8ea72d5e492fb29d3afd15cd7e082
Adam <adam.gebicki01@gmail.com>
2021-05-07 02:01:44 +0200

    Entity Framework learning
    Signed-off-by: Adam <adam.gebicki01@gmail.com>

---
+++ Restaurant/RestaurantSeeder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Restaurant.Entities;
+
+namespace Restaurant
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurant.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurant.AddRange(restaurants);
+                }
+            }
+        }
+
+        private IEnumerable<Entities.Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Entities.Restaurant>()
+        }
+    }
+}
